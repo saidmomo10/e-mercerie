7d2ec1532b8f5b33e0b51ecbdecf6ff9fa72988b
Mohamed <saidoumohamef2@gmail.com>
2025-11-17 11:05:29 +0100

    docke

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,14 +15,14 @@ class UserSeeder extends Seeder
             ['name' => 'Super Admin', 'password' => Hash::make('password'), 'role' => 'admin']
         );
 
-        User::firstOrCreate(
-            ['email' => 'belle@emercerie.com'],
-            ['name' => 'Mercerie Belle Couture', 'password' => Hash::make('password'), 'role' => 'mercerie']
-        );
+        // User::firstOrCreate(
+        //     ['email' => 'belle@emercerie.com'],
+        //     ['name' => 'Mercerie Belle Couture', 'password' => Hash::make('password'), 'role' => 'mercerie']
+        // );
 
-        User::firstOrCreate(
-            ['email' => 'alain@emercerie.com'],
-            ['name' => 'Couturier Alain', 'password' => Hash::make('password'), 'role' => 'couturier']
-        );
+        // User::firstOrCreate(
+        //     ['email' => 'alain@emercerie.com'],
+        //     ['name' => 'Couturier Alain', 'password' => Hash::make('password'), 'role' => 'couturier']
+        // );
     }
 }
